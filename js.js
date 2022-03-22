b09d3b7f195720ea80c8b06e506592f06daa840b
Zauq <zerakhasanhashmi@gmail.com>
2022-03-22 16:06:41 +0530

    Grid changes color on hovering.

--- js.js
+++ js.js
@@ -1,10 +1,19 @@
-function makeGrid(){
-        for(let j = 1; j<257; j++){         //removed nested loops, since is grid layouts is designed using css.
-            container.appendChild(containerChild.cloneNode(true));          //appending new div after each iteration by cloning the div each time.
-        }
+function makeGrid() {
+    for (let j = 1; j < 257; j++) {         //removed nested loops, since is grid layouts is designed using css.
+        container.appendChild(containerChild.cloneNode(true));          //appending new div after each iteration by cloning the div each time.
     }
+}
 
 let container = document.querySelector('#container');
 let containerChild = document.createElement('div');
 containerChild.classList.add("containerChild");
+
 makeGrid();         //Calling the function to create a grid. 
+
+let colorChangeCells = document.querySelectorAll(".containerChild");
+
+colorChangeCells.forEach(colorChangeCell => {           //looping through each value and adding an event listener to each individual cell.
+    colorChangeCell.addEventListener('mouseover', function () {           //for some reason, figuring this out took an hour.
+        this.style.backgroundColor = 'red';         //changes colour everytime mouse hovers over it.
+    });
+});
