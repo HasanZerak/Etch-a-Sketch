bbd1107de14f89b859f6412ec004c94700b1e655
Zauq <zerakhasanhashmi@gmail.com>
2022-03-22 22:26:27 +0530

    completed btnReset(), grid can now be cleared and a new grid can be added.

--- js.js
+++ js.js
@@ -1,27 +1,26 @@
-function makeGrid() {
-    for (let j = 1; j < 257; j++) {         //removed nested loops, since is grid layouts is designed using css.
-        container.appendChild(containerChild.cloneNode(true));          //appending new div after each iteration by cloning the div each time.
+let container = document.querySelector('#container');
+
+function makeGrid(newGridSize) {            //function to create new grid.
+    container.style.gridTemplateRows = `repeat(${newGridSize}, 1fr)`;           //rows of grid.
+    container.style.gridTemplateColumns = `repeat(${newGridSize}, 1fr)`;            //columns of grid.
+
+    for (let j = 0; j < (newGridSize * newGridSize); j++) {         //removed nested loops, since is grid layouts is designed using css.
+        let containerChild = document.createElement('div');         //creating a new div.
+        containerChild.classList.add("containerChild");            //adding a class to each new div.
+        //adding an event listener to each div.
+        containerChild.addEventListener('mouseover', changeColor);           //for some reason, figuring this out took an hour.
+        container.appendChild(containerChild)          //appending new div after each iteration by cloning the div each time.
     }
 }
 
-
-
-let container = document.querySelector('#container');
-let containerChild = document.createElement('div');
-containerChild.classList.add("containerChild");
-
-makeGrid();         //Calling the function to create a grid. 
-
-let colorChangeCells = document.querySelectorAll(".containerChild");
-
-colorChangeCells.forEach(colorChangeCell => {           //looping through each value and adding an event listener to each individual cell.
-    colorChangeCell.addEventListener('mouseover', function () {           //for some reason, figuring this out took an hour.
-        this.style.backgroundColor = 'red';         //changes colour everytime mouse hovers over it.
-    });
-});
+function changeColor(e) {
+    e.target.style.backgroundColor = 'red';         //changes colour everytime mouse hovers over it.
+}
 
 function btnReset() {           //function for Reset button.
-    colorChangeCells.forEach(child => {         //looping through each element.
-        child.style.backgroundColor = '';           //removing color from each element.
-    });
+    newGridSize = prompt("Enter the new size of the grid : ");
+    container.innerHTML = '';         //removing everything from container and crearting an entirely new grid.
+    makeGrid(newGridSize);
 }
+
+makeGrid(20);         //Calling the function to create a grid. 
