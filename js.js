11345f63b1781dec0825a9a990d9cc7dd466ed62
Zauq <zerakhasanhashmi@gmail.com>
2022-03-23 21:00:51 +0530

    Added button to change and remove color of a square.

--- js.js
+++ js.js
@@ -1,5 +1,8 @@
 let container = document.querySelector('#container');
-
+let defaultColor = 'red';
+let eraser = document.querySelector('.eraser');             //storing the three buttons in variables.
+let color = document.querySelector('.color')
+let randColor = document.querySelector('.randColor');
 
 function makeGrid(newGridSize) {            //function to create new grid.
     container.style.gridTemplateRows = `repeat(${newGridSize}, 1fr)`;           //rows of grid.
@@ -9,24 +12,40 @@ function makeGrid(newGridSize) {            //function to create new grid.
         let containerChild = document.createElement('div');         //creating a new div.
         containerChild.classList.add("containerChild");            //adding a class to each new div.
         //adding an event listener to each div.
+
         containerChild.addEventListener('mouseover', changeColor);           //for some reason, figuring this out took an hour.
-        container.appendChild(containerChild)          //appending new div after each iteration by cloning the div each time.
-        // containerChild.addEventListener('mouseover', playSound2);
+        container.appendChild(containerChild)          //appending new div after each iteration.
     }
 }
 
-function changeColor(e) {
-    if (e.target.style.backgroundColor !== 'red'){          //only running the function if color of cell has not been changed.
-        e.target.style.backgroundColor = 'red';         //changes colour everytime mouse hovers over it.
-        let audio = new Audio('/home/hasan/repos/Etch-a-Sketch/sound/QKTA234-pop.mp3');         //variable to hold the audio.
-        audio.play();           //playing the audio.
-    }
+function changeColor() {
+    if(this.style.backgroundColor !== defaultColor){
+    this.style.backgroundColor = defaultColor;         //changes colour everytime mouse hovers over it.
+    let audio = new Audio('/home/hasan/repos/Etch-a-Sketch/sound/QKTA234-pop.mp3');         //variable to hold the audio.
+    audio.play();           //playing the audio.
 }
+}
+
+color.addEventListener("click", function () {           //button to change color to the default color, by changing 'defaultColor'  
+    defaultColor = 'red';                               //to the color "red".
+});
+
+randColor.addEventListener("click", function () {           //button to change color to any random color, by changing 'defaultColor'
+    let letters = '0123456789ABCDEF';                       //to any color.
+    let color = '#';                                        
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    defaultColor = color;
+});
+
+eraser.addEventListener("click", () => {            //button to change color to nothing, by changing 'defaultColor' to an empty string. 
+    defaultColor = '';
+});
 
 function btnReset() {           //function for Reset button.
     newGridSize = prompt("Enter the new size of the grid : ");
     container.innerHTML = '';         //removing everything from container and crearting an entirely new grid.
     makeGrid(newGridSize);
 }
-
 makeGrid(20);         //Calling the function to create a grid. 
